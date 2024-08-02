1e0e9b94bd44627eeba4f227d918fb1f132d1662
Tulza <40633228+tulza@users.noreply.github.com>
2024-08-03 01:11:42 +1200

    fixed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { Link, Route, Routes, useLocation } from "react-router-dom";
-
-import QRCODECOMPONENT from "./qr-code-component/QRCODECOMPONENT";
-import Home from "./home/Home";
-import { ArrowLeft } from "lucide-react";
-import { AnimatePresence, motion } from "framer-motion";
-
-type Challange = { label: string; path: string; element: React.ReactNode };
-
-export const mapChallanges: Challange[] = [
-  { label: "QRcode component", path: "/qr-code-component", element: <QRCODECOMPONENT /> },
-];
+import { Route, Routes, useLocation } from "react-router-dom";
+import Test from "./components/Test";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-        <Routes key={location.pathname} location={location}>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </>
   );
 }
