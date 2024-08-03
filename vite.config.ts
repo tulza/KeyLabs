2f797a7850ee01d980033659ef022eefecaa8eec
BreadButterCat <40633228+tulza@users.noreply.github.com>
2024-08-03 13:20:45 +1200

    Temp NextJs Template

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend-mentor-challenges/",
+  base: "/KayLabs/",
   plugins: [react(), tsconfigPaths()],
 });
