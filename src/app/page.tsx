54c06adb3b6a6b11aa03be554428cedb247c74b1
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2024-08-04 02:31:19 +1200

    Changed title to tailwind, added title to page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,21 @@ import VirtualKeyboard, { KeyboardKey } from "@/components/VirtualKeyboard";
 import HomePage from "@/components/HomePage/HomePage";
 import Link from "next/link";
 import KeyGrid from "@/components/KeyGrid";
+import Title from "@/components/Title";
 
 export default function Home() {
   return (
-    <div className="flex h-dvh w-dvw flex-col items-center justify-center overflow-hidden p-8">
-      <div className="absolute top-2 whitespace-nowrap">
-        <Timer initialTime={5} />
+    <>
+      <div className="flex h-dvh w-dvw flex-col items-left justify-left overflow-hidden p-8">
+        <div className="absolute top-2 whitespace-nowrap">
+          <Timer initialTime={5} />
+        </div>
+        <div>
+          <Title></Title>
+        </div>
+
+        {/* <KeyGrid /> */}
       </div>
-      <KeyGrid />
-    </div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,12 +18,16 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        'patrick-hand': ['"Patrick Hand"', 'cursive'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
+  
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
