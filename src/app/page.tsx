3ec95d73845acc1c97e562aee9a95fb8ed6940ad
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2024-08-03 15:03:44 +1200

    Created title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 
 import Timer from "@/components/Timer";
 import VirtualKeyboard from "@/components/VirtualKeyboard";
+import Title from "@/components/Title";
 
 export default function Home() {
   return (
-    <div className="flex h-dvh w-dvw items-center justify-center">
-      <Timer initialTime={5}/>
-      <VirtualKeyboard/>
-    </div>
+    <div>
+      <Title></Title>
+      <div className="flex h-dvh w-dvw items-center justify-center">
+        <Timer initialTime={5}/>
+        <VirtualKeyboard/>
+      </div>
+    </div>  
   );
 }
