114ab66f9b030ab96f73d7d40a3ffedcdcf8e53e
BreadButterCat <40633228+tulza@users.noreply.github.com>
2024-08-03 18:14:56 +1200

    cloudflare config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import VirtualKeyboard from "@/components/VirtualKeyboard";
 import Title from "@/components/Title";
 import Flash from "@/components/Flash";
 
+// cloudflare
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <div className="w-dvw h-dvh overflow-hidden">
