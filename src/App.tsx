3115c89d1ee3caca6411cd1509d2998a0c9e8113
minugem <closehaan@gmail.com>
2024-08-03 13:24:30 +1200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-// import VirtualKeyboard from "./components/Test";
+import VirtualKeyboard from "./components/Test";
 import Timer from "./components/Timer";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes key={location.pathname} location={location}>
-        {/* <Route path="/" element={<VirtualKeyboard />} /> */}
+        <Route path="/" element={<VirtualKeyboard />} />
         <Route path="/" element={<Timer initialTime={5}/>} />
       </Routes>
     </>
