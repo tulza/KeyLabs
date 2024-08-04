c4b3a760380259c1bbe0b38fe54a5b9ea913335e
Anton Garay <antonvince.garay@gmail.com>
2024-08-04 14:29:20 +1200

    Implemented getLeaderboard API

--- src/app/api/getLeaderboard/route.ts
+++ src/app/api/getLeaderboard/route.ts
@@ -1,1 +1,26 @@
-    
+import { NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+  try {
+    const topScores = await prisma.gameInfo.findMany({
+      orderBy: {
+        lettersPerMinute: 'desc',
+      },
+      take: 10,
+      include: {
+        player: true,
+      },
+    })
+
+    return NextResponse.json(topScores, { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Something went wrong.' },
+      { status: 500 }
+    )
+  }
+}
