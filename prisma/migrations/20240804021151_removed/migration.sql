af01a50a7bc6611971bdfc0e284a4550968eb4ec
Anton Garay <antonvince.garay@gmail.com>
2024-08-04 15:24:05 +1200

    Migrate to supabase

--- prisma/migrations/20240804021151_removed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `password` on the `Player` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Player" DROP COLUMN "password";
