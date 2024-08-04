a671d45b95d213f69854eb05ffb44896820e684d
Anton Garay <antonvince.garay@gmail.com>
2024-08-04 16:33:11 +1200

    changed / to /kgrid

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
       if (response.ok) {
         console.log('Sign In Response:', data)
         localStorage.setItem('playerName', name)
-        router.push('/')
+        router.push('/kgrid')
       } else {
         setError(data.error || 'Login failed')
         console.error(data.error || 'Login failed')
